af2c3b7dd47e160c6255b4118c0a0099361a41c4
Asil <asil.home@gmail.com>
2020-06-04 12:41:07 +0800

    Single File Component

---
+++ vue-102/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <h5 style="color: #554d3f">{{item.title}}</h5>
+      <p>{{item.body}}</p>
+    </div>
+    <div>
+      <button type="button" @click="$emit('edit-item', item)" style="margin-right: 3px">Edit</button>
+      <button type="button" @click="$emit('delete-item', item)">Delete</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"]
+};
+</script>
+
+<style>
+</style>
